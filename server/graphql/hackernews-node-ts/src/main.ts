214055525a39003f78a2b5f55e1a21759d4b1503
Lucas Wang <118956159+lucaswang977@users.noreply.github.com>
2025-03-05 06:01:59 +0000

    GraphQL learning

--- server/graphql/hackernews-node-ts/src/main.ts
+++ server/graphql/hackernews-node-ts/src/main.ts
@@ -1,19 +1,13 @@
-import { execute, parse } from "graphql";
 import { schema } from "./schema";
+import { createServer } from "node:http";
+import { createYoga } from "graphql-yoga";
 
-async function main() {
-  const myQuery = parse(/* GraphQL */ `
-    query {
-      hello
-    }
-  `);
-
-  const result = await execute({
-    schema,
-    document: myQuery,
+function main() {
+  const yoga = createYoga({ schema });
+  const server = createServer(yoga);
+  server.listen(4000, () => {
+    console.info("Server is running on http://localhost:4000/graphql");
   });
-
-  console.log(result);
 }
 
 main();
--- server/graphql/hackernews-node-ts/src/schema.ts
+++ server/graphql/hackernews-node-ts/src/schema.ts
@@ -2,13 +2,40 @@ import { createSchema } from "graphql-yoga";
 
 const typeDefinitions = /* GraphQL */ `
   type Query {
-    hello: String!
+    info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
+type Link = {
+  id: string;
+  url: string;
+  description: string;
+};
+
+const links: Link[] = [
+  {
+    id: "link-0",
+    url: "https://graphql-yoga.com",
+    description: "The easiest way of setting up a GraphQL server",
+  },
+];
+
 const resolvers = {
   Query: {
-    hello: () => "Hello World!",
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Link: {
+    id: (parent: Link) => parent.id,
+    description: (parent: Link) => parent.description,
+    url: (parent: Link) => parent.url,
   },
 };
 
