e2a57bd238cadcc6895b51701762f7e2d21ddcde
Lucas Wang <118956159+lucaswang977@users.noreply.github.com>
2025-03-21 08:32:39 +0000

    Codepen mobile nav

--- challenges/codepen/e-commerce-landing-page-NWQeqJR/main-mock.js
+++ challenges/codepen/e-commerce-landing-page-NWQeqJR/main-mock.js
@@ -7,6 +7,29 @@ const mobileMenuCloseBtn = document.querySelector(
   "[data-mobile-menu-close-btn"
 );
 
+const accordionBtns = document.querySelectorAll("[data-accordion-btn");
+for (let btn of accordionBtns) {
+  btn.addEventListener("click", () => {
+    btn.nextElementSibling.classList.toggle("active");
+    const ionIcons = btn.querySelectorAll("ion-icon");
+    for (let icon of ionIcons) {
+      icon.classList.toggle("hidden");
+    }
+
+    for (let otherBtn of accordionBtns) {
+      if (otherBtn !== btn) {
+        if (otherBtn.nextElementSibling.classList.contains("active")) {
+          otherBtn.nextElementSibling.classList.remove("active");
+          const ionIcons = otherBtn.querySelectorAll("ion-icon");
+          for (let icon of ionIcons) {
+            icon.classList.toggle("hidden");
+          }
+        }
+      }
+    }
+  });
+}
+
 mobileMenuOpenBtn.addEventListener("click", () => {
   overlay.classList.add("active");
   mobileMenu.classList.add("active");
