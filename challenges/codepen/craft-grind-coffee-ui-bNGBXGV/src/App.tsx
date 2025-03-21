74fa7b073e7ba3986d7f9ae3c3d5aa610dc5722a
Lucas Wang <118956159+lucaswang977@users.noreply.github.com>
2025-03-21 09:04:38 +0000

    Codepen mock, prepare to rewrite with TW

--- challenges/codepen/craft-grind-coffee-ui-bNGBXGV/src/App.tsx
+++ challenges/codepen/craft-grind-coffee-ui-bNGBXGV/src/App.tsx
@@ -1,20 +1,34 @@
+// https://codepen.io/GemmaCroad/pen/bNGBXGV
+
 import "./App.css";
+
+const Logo = () => {
+  return (
+    <h2 className="logo">
+      <span className="title-light">craft</span>
+      <span className="title-secondary">&grind</span>
+    </h2>
+  );
+};
+
+const NavMenu = () => {
+  return (
+    <ul className="header-menu">
+      <li className="header-menu-item">home</li>
+      <li className="header-menu-item">shop</li>
+      <li className="header-menu-item">about</li>
+      <li className="header-menu-item">subscriptions</li>
+      <li className="header-menu-item">blog</li>
+    </ul>
+  );
+};
 
 function App() {
   return (
     <main className="main">
       <header className="header-main">
-        <h2 className="header-title">
-          <span className="header-title-light">craft</span>
-          <span className="header-title-secondary">&grind</span>
-        </h2>
-        <ul className="header-menu">
-          <li className="header-menu-item">home</li>
-          <li className="header-menu-item">shop</li>
-          <li className="header-menu-item">about</li>
-          <li className="header-menu-item">subscriptions</li>
-          <li className="header-menu-item">blog</li>
-        </ul>
+        <Logo />
+        <NavMenu />
         <div className="header-links">
           <button className="light-outline-btn">login</button>
           <button className="accent-filled-btn">shop now</button>
